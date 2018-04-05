5b50ed632a4ae99423ecdda9f641fc337b308f7b
Ichiro <suzukiichiro05@gmail.com>
2018-04-04 19:01:06 -0700

    first commit

--- Cookie Clicker/ViewController.swift
+++ Cookie Clicker/ViewController.swift
@@ -6,11 +6,18 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    var cookies: Float = 0;
+    var cookiesPerSec: Float = 0.6;
+    
+    @IBOutlet var cookieNumberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: Selector("cookiesPerSecond"), userInfo: nil, repeats: true)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +26,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func pressCookie(sender: AnyObject){
+        addCookies(1)
+    }
+    
+    func addCookies(_ cooks:Float){
+        cookies += cooks
+        cookieNumberLabel.text = "Cookies: \(floor(cookies))";
+        cookieNumberLabel.sizeToFit();
+    }
+    
+    @objc
+    func cookiesPerSecond(){
+        addCookies(cookiesPerSec)
+    }
 }
 
